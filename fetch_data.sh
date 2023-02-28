8359ddbb8788261faa6ad323738684191e6655d4
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2023-02-28 13:28:52 +0100

    ignore robots, added useragent to wget

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/bash
 
 url="https://www.ncei.noaa.gov/data/global-hourly/access/"
-years=$(seq 2000 2023)
+years=$(seq 2000 2000) #TODO: Maybe parse cmdline args for a span of years and create sequence from that?
 
 # init stage
 mkdir -p data
 
 for i in $years; do
     curr_dir="${url}/${i}/"
-    wget --show-progress --no-host-directories --no-directories --quiet --random-wait --no-parent -r --reject="index.html*,*.txt" --directory-prefix="data/$i" "$curr_dir"
+    wget -e robots=off -U mozilla --show-progress --no-host-directories --no-directories --quiet --random-wait --no-parent -r --reject="index.html*,*.txt" --directory-prefix="data/$i" "$curr_dir"
     #TODO: Maybe add command to put directory onto HDFS here or create separate script for that?
 done
 
-echo "done"
+total_data_size=$(du -h data | tail -n 1 | awk '{print $1}')
+
+echo "Done downloading, total size of dataset is ${total_data_size}"
