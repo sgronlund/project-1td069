37b26b2874c750d88e6494e0435544199d135673
Sebastian Grönlund <sebastiangronlundwork@gmail.com>
2023-03-06 10:25:01 +0100

    prettifying output

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/bash
 
 url="https://www.ncei.noaa.gov/data/global-hourly/access/"
-years=$(seq 2000 2000) #TODO: Maybe parse cmdline args for a span of years and create sequence from that?
+year_start=2000
+year_end=2023
+years=$(seq $year_start $year_end) #TODO: Maybe parse cmdline args for a span of years and create sequence from that?
 
 # init stage
 mkdir -p data
 
 for i in $years; do
     curr_dir="${url}/${i}/"
-    wget -e robots=off -U mozilla --show-progress --no-host-directories --no-directories --quiet --random-wait --no-parent -r --reject="index.html*,*.txt" --directory-prefix="data/$i" "$curr_dir"
-    #TODO: Maybe add command to put directory onto HDFS here or create separate script for that?
+    echo "Fetching all files for year $i"
+    files=$(mech-dump --link $curr_dir | grep .csv)
+    j=1
+    for file in $files; do
+      wget -e robots=off -U mozilla --no-clobber --no-host-directories --no-directories --quiet --random-wait --no-parent --reject="index.html*,*.txt" --directory-prefix="data/$i" "$curr_dir/$file"
+      printf "Downloaded file $j/${#files} \r"
+      j=$((++j))
+      if [[ $j -eq 50 ]] 
+      then
+        break
+      fi
+    done
+    printf "\n"
+#TODO: Maybe add command to put directory onto HDFS here or create separate script for that?
 done
 
-python3 src/preprocess "data/"
+#python3 src/preprocess.py  "data/"
 
 
 total_data_size=$(du -h data | tail -n 1 | awk '{print $1}')
--- src/preprocess.py
+++ src/preprocess.py
@@ -5,18 +5,17 @@ from pathlib import Path
 
 def main():
     n_args = len(sys.argv)
-    if(n_args > 1):
-        dirpath = sys.argv[1]
-        direc = Path(dirpath).glob("*/*.csv")
-        for item in direc:
-            print(item.name)
-            content = item.read_text()
-            #^[a-zA-Z0-9,.+-/]
-            new_content = re.sub('\"','',content)
-            print(new_content.find('\"') >= 0)
-            item.write_text(new_content)
+    if(n_args > 2):
+        year_start = sys.argv[1]
+        year_end = sys.argv[2]
+        for year in range(year_start, year_end):
+            direc = Path("data/").glob(f"{year}/*.csv")
+            for item in direc:
+                content = item.read_text()
+                new_content = re.sub('\"','',content)
+                item.write_text(new_content)
     else:
-        print("usage: preprocess.py \"pathToDirectory\"")
+        print("usage: preprocess.py \"yearStart\" \"yearEnd\"")
 
 if __name__ == "__main__":
     main()
