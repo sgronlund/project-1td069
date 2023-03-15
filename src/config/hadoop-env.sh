848d6dac559eebf9b56f4c7b0a8c63981b7eaa54
“shermanlian” <shermanlian@163.com>
2023-03-15 21:03:41 +0100

    add hadoop config files and docker files

---
+++ src/config/hadoop-env.sh
@@ -0,0 +1,12 @@
+export JAVA_HOME=/opt/bitnami/java
+export HADOOP_HOME=/opt/hadoop
+export HADOOP_MAPRED_HOME=/opt/hadoop
+export HADOOP_CONF_DIR=/opt/hadoop/etc/hadoop
+export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib"
+
+export HDFS_NAMENODE_USER="root"
+export HDFS_DATANODE_USER="root"
+export HDFS_SECONDARYNAMENODE_USER="root"
+export YARN_RESOURCEMANAGER_USER="root"
+export YARN_NODEMANAGER_USER="root"
